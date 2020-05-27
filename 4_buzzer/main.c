e7e5ff90d098281b3718195f886f425c36d8fcfb
xieyipeng <3239202719@qq.com>
2020-05-27 11:53:33 +0800

    到此为止。。。

--- 4_buzzer/main.c
+++ 4_buzzer/main.c
@@ -3,25 +3,16 @@
 typedef unsigned char u8;
 typedef unsigned int u16;
 
-#define led P2
+sbit beep=P1^5;
 
 void delay(u16 i){	  //i==1,10Œ¢√Î
 	while(i--);
 }
 
 void main(){
-	u8 i=0;
-	led=0xfe;	//1111 1110
-	delay(50000);
+	
 	while(1){
-		for(i=0;i<7;i++){
-			led=_crol_(led,1);		  //0xfe 1111 1101
-			delay(50000);
-		}
-		for(i=0;i<7;i++){
-			led=_cror_(led,1);		 
-			delay(50000);
-		}
-		//led=0xfe;
+		//beep=~beep;
+		delay(100);
 	}
 }
